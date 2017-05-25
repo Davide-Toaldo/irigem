1b48aa9fe561bb7ccf6c3077239ed786c9278035
Rosangeles <info@rosangeles.com>
2017-05-25 10:49:03 +0200

    Aggiunta la query di inserimento dati.

--- db-test/connessione.php
+++ db-test/connessione.php
@@ -1,13 +1,41 @@
 <?php
 
-  // host, username, password, database name.
-  $mysqli = new mysqli('localhost', 'biblioteca', 'biblio', 'biblios');
+  $servername = "localhost";
+  $username = "biblioteca";
+  $password = "biblio";
+  $dbname = "biblios";
 
-    if ($mysqli->connect_error) {
-        die('Errore di connessione (' . $mysqli->connect_errno . ') '
-        . $mysqli->connect_error);
-    } else {
-        echo 'Connesso. ' . $mysqli->host_info . "\n";
-    }
+  /**
+   * CONNESSIONE AL DB
+   */
+  // host, username, password, database name.
+  $mysqli = new mysqli($servername, $username, $password, $dbname);
+
+  if ($mysqli->connect_error) {
+      die('Errore di connessione (' . $mysqli->connect_errno . ') '
+      . $mysqli->connect_error);
+  } else {
+      echo 'Connesso. ' . $mysqli->host_info . "\n";
+
+      /**
+       * INSERIMENTO DATI
+       */
+      $sql_query = "INSERT INTO libri (codice, titolo, autore) VALUES ('8678UDH', 'Pinocchio', 'Collodi')";
+
+      /**
+       * ESEGUO LA QUERY.
+       */
+      if ($mysqli->query($sql_query) === TRUE) {
+        echo "New record created successfully";
+      } else {
+          echo "Error: " . $sql . "<br>" . $mysqli->error;
+      }
+
+    /**
+     * CHIUDERE LA CONNESSIONE AL DB.
+     */
+    $mysqli->close();
+
+  }
 
  ?>
